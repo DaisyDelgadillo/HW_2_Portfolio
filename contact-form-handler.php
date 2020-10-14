f70153df555531a07e11437c1e6b367708b7ed17
DaisyDelgadillo <daisydelgadillo97@gmail.com>
2020-10-13 22:01:55 -0700

    first commit

---
+++ contact-form-handler.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>addContact.html</title>
+ <link rel = "stylesheet"
+  type = "text/css"
+  href = "contact.html" />
+</head>
+<body>
+
+<?php
+ $name = $_POST['name'];
+ $visitor_email = $_POST['email'];
+ $message = $_POST['message'];
+
+ $email_from = 'yourname@yourwebsite.com';
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+
+                            $to = "daisydelgadillo97@gmail.com";
+
+                            $headers = "From: $email_from \r\n";
+                          
+                            $headers .= "Reply-To: $visitor_email \r\n";
+                          
+                            mail($to,$email_subject,$email_body,$headers);
+?>
+</body>
+</html>
